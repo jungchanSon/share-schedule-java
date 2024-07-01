6baf93071dcfa41a8428fa0a999a6410621d4a35
sonjungchan <rnrmfjc@gmail.com>
2024-07-01 20:01:08 +0900

    init

---
+++ src/main/java/com/schedule/share/ShareScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.schedule.share;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareScheduleApplication.class, args);
+	}
+
+}
