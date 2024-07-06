dab229fac91f98f364e28f389ddd1c93454bc79b
sonjungchan <rnrmfjc@gmail.com>
2024-07-06 21:24:57 +0900

    [build] user-202407041901 | 아웃바운드 아답터 추가

---
+++ src/main/java/com/schedule/share/user/adaptor/outbound/UserCommandAdapter.java
@@ -0,0 +1,36 @@
+package com.schedule.share.user.adaptor.outbound;
+
+import com.schedule.infra.rdb.repository.UserRepository;
+import com.schedule.share.user.application.port.outbound.UserCommandPort;
+import com.schedule.share.user.domain.User;
+import com.schedule.share.user.domain.mapper.UserMapper;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UserCommandAdapter implements UserCommandPort {
+
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    @Override
+    public Long create(User user) {
+
+        return userRepository.save(userMapper.userToEntity(user)).getId();
+    }
+
+    @Override
+    public void update(User user) {
+
+        userRepository.save(userMapper.userToEntity(user));
+    }
+
+    @Override
+    public void delete(Long id) {
+
+        userRepository.deleteById(id);
+    }
+}
---
+++ src/main/java/com/schedule/share/user/adaptor/outbound/UserQueryAdaptor.java
@@ -0,0 +1,39 @@
+package com.schedule.share.user.adaptor.outbound;
+
+import com.schedule.infra.rdb.entity.UserEntity;
+import com.schedule.infra.rdb.repository.UserRepository;
+import com.schedule.share.user.application.port.outbound.UserQueryPort;
+import com.schedule.share.user.domain.User;
+import com.schedule.share.user.domain.mapper.UserMapper;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+@Component
+@AllArgsConstructor
+public class UserQueryAdaptor implements UserQueryPort {
+
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    @Override
+    public User findById(Long id) {
+
+        UserEntity user = userRepository.findById(id).get();
+
+        return userMapper.userEntityToDomain(user);
+    }
+
+    @Override
+    public List<User> findAll() {
+
+        List<User> userList = userRepository.findAll().stream().map(
+                userMapper::userEntityToDomain
+        ).toList();
+
+        return userList;
+    }
+}
