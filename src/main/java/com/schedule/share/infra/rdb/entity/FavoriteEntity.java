7adf5c7be429ebb40664d9d2f6c68f59283a9ad2
sonjungchan <rnrmfjc@gmail.com>
2024-07-11 23:35:59 +0900

    [feature] user-202407102027 | Favorite 엔티티, 레포지토리 추가

---
+++ src/main/java/com/schedule/share/infra/rdb/entity/FavoriteEntity.java
@@ -0,0 +1,45 @@
+package com.schedule.share.infra.rdb.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Table(catalog = "schedule", name = "favorite_schedule")
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder(toBuilder = true)
+public class FavoriteEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private long userId;
+
+    private long scheduleId;
+
+    private long calendarId;
+
+    private int isAllday;
+
+    private LocalDateTime scheduleStartDatetime;
+
+    private LocalDateTime scheduleEndDatetime;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+}
