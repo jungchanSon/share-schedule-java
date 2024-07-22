72ec50ab96d917dde87a0250d8a55441b1039dfe
sonjungchan <rnrmfjc@gmail.com>
2024-07-23 01:00:03 +0900

    [feature] user-202407171855 | Jwt(Access, refresh token) 프로바이더 추가

---
+++ src/main/java/com/schedule/share/common/util/JwtUtil.java
@@ -0,0 +1,48 @@
+package com.schedule.share.common.util;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    @Value("${jwt.SECRET}")
+    private String SECRET;
+    @Value("${jwt.ACCESS_EXP_MS}")
+    private int ACCESS_EXP_MS;
+    @Value("${jwt.REFRESH_EXP_MS}")
+    private int REFRESH_EXP_MS;
+
+    public String generateAccessToken(String ci) {
+        Date now = new Date();
+
+        return Jwts.builder()
+                .claim("ci", ci)
+                .subject("accessToken")
+                .issuedAt(now)
+                .expiration(new Date(now.getTime() + ACCESS_EXP_MS))
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    public String generateRefreshToken(String ci) {
+        Date now = new Date();
+
+        return Jwts.builder()
+                .claim("ci", ci)
+                .subject("refreshToken")
+                .issuedAt(now)
+                .expiration(new Date(now.getTime() + REFRESH_EXP_MS))
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    private SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(SECRET.getBytes());
+    }
+
+}
