d0ec649cddf7468fd1d3524c741e851c4e418054
sonjungchan <rnrmfjc@gmail.com>
2024-08-28 19:19:08 +0900

    [feat] user-202408060832 | config 및 도커 추가

--- src/main/java/com/schedule/share/ShareScheduleApplication.java
+++ src/main/java/com/schedule/share/ShareScheduleApplication.java
@@ -3,9 +3,7 @@ package com.schedule.share;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 @PropertySource("classpath:/env.yml")
 public class ShareScheduleApplication {
